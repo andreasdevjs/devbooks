95bd7f2c55b8f8aa452d4c5a2291601b9f891ee0
andreasdevjs <andreasdevjs@gmail.com>
2019-10-15 21:08:30 +0200

    añadimos boostrap, un poco de layout y 10 libros

--- devbooks/src/modules/my/app/app.js
+++ devbooks/src/modules/my/app/app.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import LightningElementWithSLDS from '../lightningElementWithSLDS/lightningElementWithSLDS';
+import { books } from '../../../resources/assets/js/books';
+import { api, track } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElementWithSLDS {
+    @track
+    mostrarMenu = false;
+
+    @track
+    books = [];
+
+    // Abre y cierra el menú
+    handleClicMenu() {
+        this.mostrarMenu = !this.mostrarMenu;
+        const menu = this.template.querySelector('.menu-button');
+        this.mostrarMenu ? menu.innerText = '-Menu' : menu.innerText = '+Menu'
+    }
+
+
+    connectedCallback() {
+        this.books = books;
+    }
+
+
+
+
+
+}
